1fa138d398d20f00d3914b546eda48724db368d7
Alvin Ryanputra <alvin.ryanputra@gmail.com>
2024-12-19 14:57:29 -0800

    some_function_4

--- bubble_sort.py
+++ bubble_sort.py
@@ -42,3 +42,11 @@ def some_function_3():
     for i in range(200):
         x += i
     return x
+
+def some_function_4():
+    x = 4
+    for i in range(100):
+        print(i)
+    for i in range(200):
+        x += i
+    return x
